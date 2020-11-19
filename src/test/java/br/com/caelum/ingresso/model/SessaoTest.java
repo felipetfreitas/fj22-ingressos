a6b52375e4549f1cb789b156f011a1e10a417df5
Felipe <felipet.freitas@hotmail.com>
2020-11-19 16:03:20 -0300

    Alterações na Sessao Teste

--- src/test/java/br/com/caelum/ingresso/model/SessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/SessaoTest.java
@@ -1,5 +1,9 @@
 package br.com.caelum.ingresso.model;
 
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+
 import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalTime;
@@ -10,18 +14,18 @@ import junit.framework.Assert;
 
 public class SessaoTest {
 	
-	@Test
+/*	@Test
 	public void oPrecoDaSessaoDeveSerIgualASomaDoPrecoDaSalaMaisOPrecoDoFilme() {
 		
 	Sala sala = new Sala("Eldorado - IMax", new BigDecimal("22.5"));
-	Filme filme = new Filme("Rogue One", Duration.ofMinutes(120),			
-					"SCI-FI", new BigDecimal("12.0"));
+	Filme filme = new Filme("Rogue One", Duration.ofMinutes(120),"SCI-FI", new BigDecimal("12.0"));
 	
 	BigDecimal somaDosPrecosDaSalaEFilme = sala.getPreco().add(filme.getPreco());
 	
 	Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
 	
 	Assert.assertEquals( somaDosPrecosDaSalaEFilme, sessao.getPreco() );
-	}
+	
+	}*/
 
 }
